d1806e9e2e57db9d8df607d3ad9963f6fd2f75ca
Marshall Rose <mrose17@gmail.com>
2020-01-23 09:01:36 -0500

    clean-up, parse one...

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # igbgd
-This repository was cloned from [bat-ledger](https://github.com/brave-intl/bat-ledger).
-The reason is that there is much useful "middleware" in the [bat-utils directory](tree/master/bat-utils).
+This repository was cloned from [bat-ledger](https://github.com/brave-intl/bat-ledger) --
+there is *many* useful "middleware" components in the [bat-utils directory](tree/master/bat-utils).
 
-# Installation
+# Desktop Installation
 You will need to have node, mongodb, and redis on your system.
 Then, install all the node packages:
 
@@ -19,7 +19,7 @@ Then run these commands:
     % brew install redis
     % brew services start redis
 
-# Configuration
+## Configuration
 To run locally, create a file called `.env`:
 
     % cp .env.example .env
@@ -27,13 +27,125 @@ To run locally, create a file called `.env`:
 Do not worry about any line that starts with `"x"`
 
 The one line you do need to worry about is the definition of `TOKEN_LIST` --
-this is a string that must be present in the HTTP header when creating a new entry:
+this is a string that must be present in the HTTP header when using the API:
 
     Authorization: Bearer ...
     
 where `"..."` is the value of `TOKEN_LIST`.
-You can get a fresh UUID by going [here](https://www.uuidgenerator.net/).
+You can get a fresh UUIDv4 by going [here](https://www.uuidgenerator.net/),
+although depending on your level of caution,
+you may choose to generate your own pseudo-random string.
 
-# Running
+## Running
 
     npm run start
+
+# Heroku Installation
+
+# API
+
+## Create an entry
+
+Supply the `privateID` (a new UUIDv4):
+
+    curl -X POST "http://127.0.0.1:3004/v1/entry" \
+      -H "accept: application/json"               \
+      -H "Content-Type: application/json"         \
+      -H "Authorization: Bearer ..."
+      -d "{ \"privateID\"   : \"b2d25937-14f1-48b1-9cf3-7cfeb17b13dd\"
+          , \"description\" : \"CEC HQ\"
+          , \"category\"    : \"green-iguana\"
+          , \"location\"    : { \"longitude\": 19.299706, \"latitude\": -81.381807 }
+          , \"image\"       : { \"data\": insert-the-contents-of-documentation/igbgd-base64.txt  }
+          }"
+
+Mandatory payload:
+
+- `privateID` must be a new [UUIDv4](https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random));
+
+- `category` (must be `"green-iguana"`);
+
+- `location.longitude` and  `location.latitude`; and,
+
+- `image.data` must be a [PNG](https://en.wikipedia.org/wiki/Portable_Network_Graphics) file encoded using
+[base64](https://en.wikipedia.org/wiki/Base64)
+
+
+Optional payload:
+
+- `description`
+
+On success,
+the server stores the entry and returns the `publicID`:
+
+    {
+      "publicID": "77d29272-25c4-4521-ba1a-e33342d8f04b"
+    }
+
+The application should persist the `privateID` it creates,
+as some operations require this.
+
+The application should also persist the corresponding `publicID` the server creates,
+so entries may be displayed as either created by this user or another user.
+
+# Find nearby entries
+
+Supply the geolocation and the radius (e.g., 2000 meters):
+
+    curl -X GET "http://127.0.0.1:3004/v1/entries/circle/2000?longitude=19.348461&latitude=-81.381988"  \
+      -H "accept: application/json"                                                                     \
+      -H "Authorization: Bearer ..."
+
+Mandatory parameters:
+
+- longitude and latitude; and,
+
+- radius (in meters)
+
+Optional parameters:
+
+- `category` (must be `"green-iguana"`); and,
+
+- `limit` on the number of returned entries (must not exceed 25)
+
+On success,
+the server returns an array of entries:
+
+    [
+      { "publicID"    : "77d29272-25c4-4521-ba1a-e33342d8f04b"
+      , "description" : "CEC HQ"
+      , "category"    : "green-iguana"
+      , "location"    :
+        { "longitude" : 19.299706
+        , "latitude"  : -81.381807
+        },
+      , "image":
+         { "data"     : "..."
+         , "format"   : "png"
+        }
+      }
+    ]
+
+# Retrieve an entry
+
+Supply the `privateID`:
+
+    curl -X GET "http://127.0.0.1:3004/v1/entry/b2d25937-14f1-48b1-9cf3-7cfeb17b13dd" \
+      -H "accept: application/json"                                                   \
+      -H "Authorization: Bearer ..."
+
+On success,
+the server returns that entry:
+
+    { "publicID"    : "77d29272-25c4-4521-ba1a-e33342d8f04b"
+    , "description" : "CEC HQ"
+    , "category"    : "green-iguana"
+    , "location"    :
+      { "longitude" : 19.299706
+      , "latitude"  : -81.381807
+      },
+    , "image":
+       { "data"     : "..."
+       , "format"   : "png"
+      }
+    }
--- igbgd/controllers/entry.js
+++ igbgd/controllers/entry.js
@@ -2,128 +2,76 @@ const Joi = require('@hapi/joi')
 const boom = require('boom')
 const bson = require('bson')
 const underscore = require('underscore')
-const SDebug = require('sdebug')
 const uuidV4 = require('uuid/v4')
 
 const utils = require('bat-utils')
 const braveHapi = utils.extras.hapi
-// const braveJoi = utils.extras.joi
-
-const debug = new SDebug('igbgd')
 
 const v1 = {}
 
 const joikeys = {
+  category: Joi.string().valid('green-iguana'),
+  entry: {
+    publicID: Joi.string().guid().required().description('public identity of the entry'),
+    privateID: Joi.string().guid().required().description('private identity of the entry'),
+    description: Joi.string().optional()
+  },
   image: {
     data: Joi.string().base64({ paddingRequired: false }).description('base64 encoding'),
     format: Joi.string().regex(/^png$/).default('png')
   },
-  limit: Joi.number().positive().optional().description('the maximum number of entries to return'),
+  limit: Joi.number().positive().max(25).optional().description('the maximum number of entries to return'),
   location: {
     longitude: Joi.number().precision(8).min(-180).max(180).required().description('east-west'),
     latitude: Joi.number().precision(8).min(-90).max(90).required().description('north-south'),
-    elevation: Joi.number().optional().description('in meters')
-  }
+    elevation: Joi.number().precision(3).optional().description('in meters')
+  },
+  radius: Joi.number().positive().max(10000).description('in meters'),
+  shape: Joi.string().regex(/^circle$/).required()
 }
-joikeys.entry = {
-  publicID: Joi.string().guid().required().description('public identity of the entry'),
-  privateID: Joi.string().guid().required().description('private identity of the entry'),
-  category: Joi.string().regex(/^green-iguana$/).required(),
+underscore.extend(joikeys.entry, {
+  category: joikeys.category.required(),
   location: Joi.object().keys(joikeys.location).required(),
-  image: Joi.object().keys(joikeys.image).required(),
-  description: Joi.string().optional()
-}
+  image: Joi.object().keys(joikeys.image).required()
+})
 
 /*
-  GET /v1/entry/{shape}/{radius}
+  GET /v1/enties/{shape}/{radius}
 */
 
 v1.getEntries = {
   handler: (runtime) => {
     return async (request, h) => {
       const radius = request.params.radius
-      const params = request.query
-      const query = {
+      const query = request.query
+      const find = {
         location: {
           $near: {
-            $geometry: { type: 'Point', coordinates: [ params.longitude, params.latitude ] },
-            $minDistance: 0,
+            $geometry: { type: 'Point', coordinates: [ query.longitude, query.latitude ] },
             $maxDistance: radius
           }
         }
       }
-      const entries = runtime.database.get('entries', debug)
 
-      let limit = parseInt(params.limit, 10)
+      const category = query.category
+      if (category !== undefined) find.category = category
+
+      let limit = parseInt(query.limit, 10)
       if (isNaN(limit) || (limit > 512)) limit = 512
 
-      console.log('!!! query=' + JSON.stringify(query, null, 2))
-      const matches = await entries.find(query, { limit: limit })
-      console.log('!!! matches=' + JSON.stringify(matches, null, 2))
+      const debug = braveHapi.debug(module, request)
+      const entries = runtime.database.get('entries', debug)
 
-      const keys = underscore.keys(underscore.omit(joikeys.entry, [ 'privateID' ]))
+      debug('!!! find=', find)
+      const matches = await entries.find(find, { limit: limit })
+      debug('!!! matches=', matches)
+
       const result = []
       matches.forEach(match => {
-        const entry = underscore.pick(match, keys)
-        const coordinates = match.location.coordinates
-
-        entry.location = { longitude: coordinates[0], latitude: coordinates[1] }
-        if (coordinates.length > 2) entry.location.elevation = coordinates[2]
-        result.push(entry)
+        result.push(m2e(match))
       })
 
       return result
-    }
-  },
-
-  description: 'Get entries within a particular geo-fence',
-  tags: [ 'api' ],
-
-  validate: {
-    params: Joi.object().keys({
-      shape: Joi.string().regex(/^circle$/).required(),
-      radius: Joi.number().positive().description('in meters')
-    }),
-    query: Joi.object().keys(underscore.extend({ limit: joikeys.limit }, joikeys.location))
-  },
-
-  response: {
-    schema: Joi.array().items(Joi.object().keys(underscore.omit(joikeys.entry, [ 'privateID' ])))
-  }
-}
-
-/*
-  POST /v1/entry
- */
-
-v1.postEntry = {
-  handler: (runtime) => {
-    return async (request, h) => {
-      const payload = request.payload
-      const privateID = payload.privateID
-      const entries = runtime.database.get('entries', debug)
-
-      let publicID = uuidV4().toLowerCase()
-      let result = await entries.findOne({ privateID: privateID })
-      if (result) throw boom.badData('private identity entry already exists: ' + privateID)
-
-      if (payload.image.format === undefined) payload.image.format = 'png'
-      try {
-        await entries.insert(underscore.extend(underscore.pick(payload, [ 'privateID', 'category', 'image', 'description' ]), {
-          publicID: publicID,
-          location: { type: 'Point', coordinates: [ payload.location.longitude, payload.location.latitude ] },
-          timestamp: bson.Timestamp()
-        }))
-      } catch (ex) {
-        runtime.notify(debug, { text: 'entries error: ' + ex.toString() })
-        debug('entries error', ex)
-        throw boom.badData(ex.toString())
-      }
-
-      result = await entries.findOne({ privateID: privateID })
-      if (!result) throw boom.badImplementation('database creation failed: ' + privateID)
-
-      return { publicID: publicID }
     }
   },
 
@@ -137,6 +85,116 @@ v1.postEntry = {
   tags: [ 'api' ],
 
   validate: {
+    params: Joi.object().keys({
+      shape: joikeys.shape,
+      radius: joikeys.radius
+    }),
+    query: Joi.object().keys(underscore.extend({
+      category: joikeys.category.optional(),
+      limit: joikeys.limit
+    }, joikeys.location))
+  },
+
+  response: {
+    schema: Joi.array().items(Joi.object().keys(underscore.omit(joikeys.entry, [ 'privateID' ])))
+  }
+}
+
+const m2e = (match) => {
+  const keys = underscore.keys(underscore.omit(joikeys.entry, [ 'privateID' ]))
+  const entry = underscore.pick(match, keys)
+  const coordinates = match.location.coordinates
+
+  entry.location = { longitude: coordinates[0], latitude: coordinates[1] }
+  if (coordinates.length > 2) entry.location.elevation = coordinates[2]
+
+  return entry
+}
+
+/*
+  GET /v1/entry/{privateID}
+ */
+
+v1.getEntry = {
+  handler: (runtime) => {
+    return async (request, h) => {
+      const privateID = request.params.privateID
+
+      const debug = braveHapi.debug(module, request)
+      const entries = runtime.database.get('entries', debug)
+
+      const match = await entries.findOne({ privateID: privateID })
+      if (!match) throw boom.notFound('no such entry: ' + privateID)
+
+      return m2e(match)
+    }
+  },
+
+  auth: {
+    strategy: 'simple-scoped-token',
+    scope: ['global'],
+    mode: 'required'
+  },
+
+  description: 'Get a particular entry using it\'s creation key',
+  tags: [ 'api' ],
+
+  validate: {
+    params: Joi.object().keys({
+      privateID: joikeys.entry.privateID
+    })
+  },
+
+  response: {
+    schema: Joi.object().keys(underscore.omit(joikeys.entry, [ 'privateID' ]))
+  }
+}
+
+/*
+  POST /v1/entry
+ */
+
+v1.postEntry = {
+  handler: (runtime) => {
+    return async (request, h) => {
+      const payload = request.payload
+      const privateID = payload.privateID
+
+      const debug = braveHapi.debug(module, request)
+      const entries = runtime.database.get('entries', debug)
+      let match = await entries.findOne({ privateID: privateID })
+      if (match) throw boom.badData('entry already exists: ' + privateID)
+
+      const publicID = uuidV4().toLowerCase()
+      try {
+        await entries.insert(underscore.extend(underscore.pick(payload, [ 'privateID', 'category', 'image', 'description' ]), {
+          publicID: publicID,
+          location: { type: 'Point', coordinates: [ payload.location.longitude, payload.location.latitude ] },
+          timestamp: bson.Timestamp()
+        }))
+      } catch (ex) {
+        runtime.notify(debug, { text: 'entries error: ' + ex.toString() })
+        debug('entries error', ex)
+        throw boom.badData(ex.toString())
+      }
+
+      match = await entries.findOne({ privateID: privateID })
+      if (!match) throw boom.badImplementation('database creation failed: ' + privateID)
+
+      return { publicID: publicID }
+    }
+  },
+
+  auth: {
+    strategy: 'simple-scoped-token',
+    scope: ['global'],
+    mode: 'required'
+  },
+
+  description: 'Create an entry',
+  tags: [ 'api' ],
+
+  validate: {
     payload: Joi.object().keys(underscore.omit(joikeys.entry, [ 'publicID' ]))
   },
 
@@ -145,10 +203,13 @@ v1.postEntry = {
   }
 }
 
-if ((process.env.NODE_ENV === 'development') && (process.env.GITHUB_FORCE_HTTPS === 'false')) delete v1.postEntry.auth
+if ((process.env.NODE_ENV === 'development') && (process.env.GITHUB_FORCE_HTTPS === 'false')) {
+  underscore.keys(v1).forEach((method) => { delete v1[method].auth })
+}
 
 module.exports.routes = [
-  braveHapi.routes.async().path('/v1/entry/{shape}/{radius}').config(v1.getEntries),
+  braveHapi.routes.async().path('/v1/entries/{shape}/{radius}').config(v1.getEntries),
+  braveHapi.routes.async().path('/v1/entry/{privateID}').config(v1.getEntry),
   braveHapi.routes.async().post().path('/v1/entry').config(v1.postEntry)
 ]
 
